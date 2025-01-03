6740f42c0aa75a625cd682770dcebe8fa2ecb902
maniraj1213 <maninonisathish@gmail.com>
2025-01-03 17:51:11 +0530

    Delete CRUDCBVProject directory

--- CRUDCBVProject/MyApps1/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from MyApps1.models import Company
-#Register your models here.
-class CompanyAdmin(admin.ModelAdmin):
-	list_display=['name', 'location','ceo'];
-
-admin.site.register(Company,CompanyAdmin)
