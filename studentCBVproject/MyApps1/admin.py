13d62df1c42f2894eab4f8b98c00be7e1bee0ac8
maniraj1213 <maninonisathish@gmail.com>
2025-01-03 17:53:18 +0530

    Delete studentCBVproject directory

--- studentCBVproject/MyApps1/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from MyApps1.models import Student
-#Register your models here.
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ['sno','sname','sheight','sage']
-
-admin.site.register(Student, StudentAdmin)
