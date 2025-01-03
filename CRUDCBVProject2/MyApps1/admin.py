2980acbd1a6e74f44a91dc293b23b678df85c756
maniraj1213 <maninonisathish@gmail.com>
2025-01-03 17:50:34 +0530

    Delete CRUDCBVProject2 directory

--- CRUDCBVProject2/MyApps1/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from MyApps1.models import Employee
-#Register your models here.
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['empno','ename','job','sal']
-
-admin.site.register(Employee, EmployeeAdmin)
