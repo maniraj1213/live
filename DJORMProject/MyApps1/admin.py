f7e785887fd004ccb4ab05ab86348d2caaddb8bb
maniraj1213 <maninonisathish@gmail.com>
2025-01-03 17:52:35 +0530

    Delete DJORMProject directory

--- DJORMProject/MyApps1/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from MyApps1.models import Employee
-
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['empno','ename','job','sal','address'];
-
-admin.site.register(Employee,EmployeeAdmin)
